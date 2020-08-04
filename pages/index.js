3778f82567654222bb1eec6ac7088eb29761cdaf
Tomasz Kopacki <tomasz@kopacki.net>
2020-08-04 14:48:53 +1000

    feat: setup index page

--- pages/index.js
+++ pages/index.js
@@ -1,60 +1,23 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>
+        Research. Develop. Ship. Web 3.0 for the best blockchain teams | Atomik
+        Labs
+      </title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1 className="title">Web 3.0: Research. Develop. Ship.</h1>
     </main>
 
     <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/vercel.svg" alt="Vercel Logo" />
-      </a>
+      <p>
+        <img src="/logo.png" alt="Atomik Labs logo" /> Atomik Labs
+      </p>
     </footer>
 
     <style jsx>{`
@@ -77,6 +40,7 @@ const Home = () => (
       }
 
       footer {
+        font-size: 1rem;
         width: 100%;
         height: 100px;
         border-top: 1px solid #eaeaea;
@@ -86,6 +50,8 @@ const Home = () => (
       }
 
       footer img {
+        max-height: 3rem;
+        vertical-align: middle;
         margin-left: 0.5rem;
       }
 
@@ -114,7 +80,21 @@ const Home = () => (
       .title {
         margin: 0;
         line-height: 1.15;
+        display: flex;
+        flex-direction: column;
+      }
+
+      h1 {
         font-size: 4rem;
+      }
+
+      h2 {
+        font-size: 3rem;
+      }
+
+      .title img {
+        align-self: center;
+        margin-top: 2rem;
       }
 
       .title,
@@ -198,6 +178,6 @@ const Home = () => (
       }
     `}</style>
   </div>
-)
+);
 
-export default Home
+export default Home;
